7fbb23d2e38fa72ea931bc29aa240ad2aff7d518
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-03-20 20:34:57 -0400

    s

--- js_file.js
+++ js_file.js
@@ -1,50 +1,20 @@
+/** Declares the variables that will be the BMR, cutting, and bulking calories **/
 var value;
 var cutting;
 var bulking;
 
-function validateInputs(){
-	/* Default return for the function */
-	var result = true;
-
-	/* Grabs the age and weight entered in the form */
-	var age = document.getElementById("age").value;
-	var weight = document.getElementById("weight").value;
-
-	/* Grabs if the male or female option was choosen in the form */
-	var male = new Boolean(document.getElementById("genderM").checked);
-	var female = new Boolean(document.getElementById("genderF").checked);
-
-	/* Enters the age and weight entered into an array */
-	var array = new Array(age, weight);
-	/* Creates a second array to be used in the for loop below */
-	var array2 = new Array("age","weight");
-
-	/* Loops through the age and weight array to check if the values entered were not empty and not a number. If they are the labels are made red*/
-		for (i=0;i<2;i++){
-			if (array[i]==null || array[i]=="" || isNaN(array[i])){
-				document.getElementById("label"+array2[i]).style.color = "red";
-				result = false;
-			}
-		}
-	/* Checks if the male or female option was choosen in the form. If not makes the label red */
-		if (male == false && female == false){
-			document.getElementById("labelgender").style.color = "red";
-			result = false;
-		}
-
-
-		return result;
-	
-}
-
 function calculator(){
 
-
+/** Declares the gender variable that is true or false if the male radio button is selected or not **/
 var gender = new Boolean(document.getElementById("genderM").checked);
+
+/** Grabs the height, age, weight, and activity level from the form **/
 var height = document.getElementById("height").value;
 var age = document.getElementById("age").value;
-var weight = document.getElementById("weight").value; 
+var weight = document.getElementById("weight").value;
 var activity = document.getElementById("activity").value;
+
+/** Declares the multiplier variable that sets to a specific number depending on what activity level was selected **/
 var multiplier;
 
 if (activity == "Sedentary"){
@@ -67,6 +37,7 @@ else {
 	multiplier = 1;
 }
 
+/** Calculates the BMR calories using the Mifflin-St Jeor equation depending on the gender of the user **/
 if (gender==true){
 	value=((10*weight+6.25*height-5*age) + 5) * multiplier;
 }
@@ -75,19 +46,19 @@ else {
 	value=((10*weight+6.25*height-5*age) - 161) * multiplier;
 }
 
+/** Determines the cutting and bulking calories by either taking away 20% from the BMR and adding 15% to the BMR **/
 cutting = value - (value*.2);
 bulking = value + (value*.15);
 }
 
-
-
-
-
+/** Creates an event lister that listens for the calculate button to be click then prints the results after the click  **/
 document.getElementsByTagName("button")[0].addEventListener("click", function() {
 	calculator(); 
 	document.getElementById('baseCals').innerHTML = value;
 	document.getElementById('cuttingCals').innerHTML = cutting;
 	document.getElementById('bulkingCals').innerHTML = bulking;
 })
+
+
 
 
